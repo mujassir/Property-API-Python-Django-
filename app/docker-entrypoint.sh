e0c7fca7f86b10559682cc0661f5faef574c7451
Mujassir Nasir <mnasir@xavor.com>
2020-09-16 20:10:31 +0500

    Config change # 14

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Collect static files
-# echo "Collect static files"
-# python3 manage.py collectstatic --noinput
+echo "Collect static files"
+python manage.py collectstatic --noinput
 
 # Apply database migrations
 # echo "Apply migrations"
@@ -11,7 +11,7 @@
 
 # Start server
 echo "Starting server"
-python src/manage.py runserver323 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:$PORT propertyapp.wsgi
 # gunicorn agencycomms.wsgi -b 0.0.0.0:8000 --workers 2 --threads 4 --timeout 240  & celery worker -A agencycomms --loglevel=info
 #celery worker -A agencycomms
 
